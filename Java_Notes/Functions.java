462733e0afcadb998b194097e2e78902b8f4bd2f
Asyraaf Mokhtar Sani <84800446+Loli-Hunter-12@users.noreply.github.com>
2021-11-11 16:03:50 +0800

    Add files via upload

---
+++ Java_Notes/Functions.java
@@ -0,0 +1,55 @@
+public class Functions 
+{
+// STRING FUNCTIONS
+    //    char | charAt(int index)               : Returns the char value at the specified index.
+
+    //     int | compareTo(String anotherString) : Compares two strings lexicographically >0 ascending, <0 descending
+    //     int | compareTo(String anotherString) : Compares two strings lexicographically, ignoring case differences.
+
+    // String  | concat(String str)              : Concatenates the specified string to the end of this string. (Concatenate = link (things) together in a chain or series.)
+    // boolean | endsWith(String suffix)         : Tests if this string ends with the specified suffix.
+
+    // boolean | equals(Object anObject)         : Compares this string to the specified object.
+    // boolean | equalsIgnoreCase(String anotherString) : Compares this String to another String, ignoring case considerations.
+
+    //     int | indexOf(int ch)                 : Returns the index within this string of the first occurrence of the specified character.
+    //     int | indexOf(int ch, int fromIndex)  : Returns the index within this string of the first occurrence of the specified character, starting the search at the specified index.
+    //     int | indexOf(String str)             : Returns the index within this string of the first occurrence of the specified substring.
+    //     int | indexOf(String str, int fromIndex) : Returns the index within this string of the first occurrence of the specified substring, starting at the specified index.
+
+    //     int | lastIndexOf(int ch)             : Returns the index within this string of the last occurrence of the specified character.
+    //     int | lastIndexOf(int ch, int fromIndex) : Returns the index within this string of the last occurrence of the specified character, searching backward starting at the specified index.
+    //     int | lastIndexOf(String str)         : Returns the index within this string of the last occurrence of the specified substring.
+    //     int | lastIndexOf(String str, int fromIndex) : Returns the index within this string of the last occurrence of the specified substring, searching backward starting at the specified index.
+
+    //  String | substring(int beginIndex)       : Returns a substring of the string starting from the index.
+    //  String | substring(int beginIndex, int endIndex) : Returns a substring of the string starting from the index and stops at the end index.
+
+    // boolean | isEmpty()                       : Returns true if, and only if, length() is 0.
+    //     int | length()                        : Returns the length of this string
+    //  char[] | toCharArray()                   : Convers this string to a new char array.
+    //  String | toLowerCase()                   : Converts all characters into lowercase
+    //  String | toUpperCase()                   : Converts all characters into uppercase
+    //  String | trim()                          : Returns a copy of the string, with leading and trailing whitespace omitted.
+
+// INT FUNCTIONS
+    //     int | compareTo(Object anotherInteger)   : Compares two integer objects numerically.
+    //     int | compare(int x, int y)              : Compares two int values numerically.
+    //  double | doubleValue()                      : Returns the value of this integer as double.
+    //   float | floatValue()                       : Returns the value of this integer as float.
+    // boolean | equals(Object obj)                 : Compares this object to the specified object.
+    // 
+
+/**
+ * private String leftpad(String text, int length) {
+    return String.format("%" + length + "." + length + "s", text);
+}
+
+private String rightpad(String text, int length) {
+    return String.format("%-" + length + "." + length + "s", text);
+}
+ */
+
+
+
+}
